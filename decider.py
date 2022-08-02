f52eafa0be58ee5a5eac90a35239749b7a7c1197
antoniolang1107 <antoniolang8@gmail.com>
2022-08-02 07:11:15 -0700

    Reads from files properly

--- decider.py
+++ decider.py
@@ -3,19 +3,21 @@ import json
 
 """
 Author: Antonio Lang
-Date: 1 July 2022
+Date: 2 August 2022
 Program: Card Rewards Decider
     Tells what the best card to use is based off a given merchant/category.
 """
 
 
 def main() -> None:
-    if len(sys.argv) == 2 is True:
-        card_data = parse_file(sys.argv[1])
-        pass
-    else:
+    if len(sys.argv) != 2:
         print(f"Incorrect number of arguments given")
-        print(f"Proper format: decider.py <cardData.json>")
+        print(f"Proper format: decider.py <card-data.txt>")
+    else:
+        card_data = []
+        for card in read_list(sys.argv[1]):
+            pass
+            card_data.append(parse_cards(card))
 
 
 def get_input() -> str:
@@ -23,10 +25,24 @@ def get_input() -> str:
     pass
 
 
-def parse_file(f_name) -> dict:
-    with open(f_name) as f:
-        data = json.load(f)
-    return data
+def parse_cards(f_name) -> dict:
+    """
+    reads the data for each card into a dictionary
+    """
+    with open(f_name) as file:
+        card_data = json.load(file)
+    return card_data
+
+
+def read_list(f_name):
+    """
+    reads the list of cards
+    """
+    cards = []
+    with open(f_name, 'r') as file:
+        while (line := file.readline().rstrip()): # reads f_names without \n
+            cards.append(line)
+    return cards
 
 
 def select_card(card_list, category) -> str:
